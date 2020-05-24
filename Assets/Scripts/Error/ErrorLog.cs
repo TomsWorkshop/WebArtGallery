9caa08fb0ded72bdd5ff8fa22ed6622995307483
calm0815(kuwano) <ku_skyline_wa@yahoo.co.jp>
2020-05-24 16:23:21 +0900

    add files

---
+++ Assets/Scripts/Error/ErrorLog.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ErrorLog : MonoBehaviour
+{
+    private void Start()
+    {
+        if (ErrorStatus.isDisconnected) showDisconnectedError();
+        if (ErrorStatus.isCreateRoomFailed) showCreateRoomFailedError();
+        if (ErrorStatus.isJoinRoomFailed) showJoinRoomFailedError();
+        if (ErrorStatus.isMasterClientSwitched) showMasterClientSwitchedError();
+        if (ErrorStatus.isLobbyStatisticsUpdate) showLobbyStatisticsUpdateError();
+    }
+
+    private void showDisconnectedError()
+    {
+        this.GetComponent<Text>().text = "Disconnected from Server.";
+        ErrorStatus.isDisconnected = false;
+    }
+
+    private void showCreateRoomFailedError()
+    {
+        this.GetComponent<Text>().text = "Failed to create room.";
+        ErrorStatus.isCreateRoomFailed = false;
+    }
+
+    private void showJoinRoomFailedError()
+    {
+        this.GetComponent<Text>().text = "Failed to join room.";
+        ErrorStatus.isJoinRoomFailed = false;
+    }
+
+    private void showMasterClientSwitchedError()
+    {
+        this.GetComponent<Text>().text = "Room Owner leaved.";
+        ErrorStatus.isMasterClientSwitched = false;
+    }
+
+    private void showLobbyStatisticsUpdateError()
+    {
+        this.GetComponent<Text>().text = "Loby Status was updated.";
+        ErrorStatus.isLobbyStatisticsUpdate = false;
+    }
+}
