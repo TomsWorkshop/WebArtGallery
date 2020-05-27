03b880ddf6497a9ae7af94ff19bde76dab6d973a
chii777 <ma_ksks40x25@yahoo.co.jp>
2020-05-27 21:43:30 +0900

    collision
    art collision

---
+++ Assets/Scripts/Model/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+    private Text targetText;
+
+    
+        void OnCollisionStay(Collision collision)
+        {
+          
+            Debug.Log("hit");
+            //文章を画面上に表示する
+            this.targetText = this.GetComponent<Text>();
+            this.targetText.text = "ChangeText";
+    }
+    
+}
---
+++ Assets/Scripts/Model/description1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class description1 : MonoBehaviour
+{
+    private Text targetText;
+
+    void substitude()
+    {
+        this.targetText = this.GetComponent<Text>();
+        this.targetText.text = "ChangeText";
+    }
+}
