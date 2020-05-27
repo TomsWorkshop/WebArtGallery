9b7ae5a8631d96819eb3b07fd0f71a61084f5baa
chii777 <ma_ksks40x25@yahoo.co.jp>
2020-05-28 07:26:11 +0900

    add text change with collision
    下記機能を追加しました。
    
    ・絵に衝突したときテキストが表示(または表示内容の変更)
    ・絵から離れるとテキストが非表示

--- Assets/Scripts/Model/PlayerController.cs
+++ Assets/Scripts/Model/PlayerController.cs
@@ -10,13 +10,18 @@ public class PlayerController : MonoBehaviour
     public Text targetText;
 
     
-        void OnCollisionStay(Collision collision)
-        {
+    void OnCollisionStay(Collision collision)　　　　　　//物体に衝突している間
+    {
           
-            Debug.Log("hit");
-            //文章を画面上に表示する
+            Debug.Log("hit");     　//consoleに表示
            // this.targetText = this.GetComponent<Text>();
-            this.targetText.text = "ChangeText";
+            this.targetText.text = "ChangeText";　　　 //表示したい文章
     }
-    
+
+    void OnCollisionExit(Collision collision)　　　　　//物体から離れたとき
+    {
+
+        this.targetText.text = " ";　　　　　　　　　　　//textに空白を入力
+
+    }
 }
