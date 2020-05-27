d877bbf9475a3f491bc3f0507b6e3949b73a96eb
calm0815(kuwano) <kuwano.tomohiro455@mail.kyutech.jp>
2020-05-27 20:35:18 +0900

    mobile sensor test

---
+++ Assets/Scripts/UI/Debug/getParam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getParam : MonoBehaviour
+{
+    private Vector3 m_accel;
+    private Gyroscope m_gyro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_accel = Input.acceleration;
+        m_gyro  = Input.gyro;
+        m_gyro.enabled = true;
+    }
+
+    private void Update()
+    {
+        m_accel = Input.acceleration;
+        m_gyro  = Input.gyro;
+    }
+
+    private void OnGUI() {
+        var AccRect = new Rect(30, 30, 500, 50);
+        var GyroRect = new Rect(30,60, 500, 50);
+        var AttitudeRect = new Rect(30,90, 500, 50);
+        GUI.skin.label.fontSize = 20;
+        GUI.Label(AccRect, string.Format("Acceleration : X={0:F2}, Y={1:F2}, Z={2:F2}",
+            m_accel.x, m_accel.y, m_accel.z));
+        GUI.Label(GyroRect, string.Format("Gyro : X={0:F2}, Y={1:F2}, Z={2:F2}",
+            m_gyro.rotationRate.x, m_gyro.rotationRate.y, m_gyro.rotationRate.z));
+        GUI.Label(AttitudeRect, string.Format("Attitude : X={0:F2}, Y={1:F2}, Z={2:F2}",
+            m_gyro.attitude.x, m_gyro.attitude.y, m_gyro.attitude.z));
+    }
+}
---
+++ Assets/Scripts/UI/main/CameraJointController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraJointController : MonoBehaviour
+{
+    //プレイヤーを変数に格納
+    private GameObject CameraObject;
+    public float rotateSpeed = 3.0f;
+
+    void Start ()
+    {
+        CameraObject = this.gameObject;
+    }
+
+    void Update ()
+    {
+        float angle = Input.GetAxis("Horizontal") * rotateSpeed;
+        // float angle = Input.acceleration.z * rotateSpeed;
+        Vector3 CameraObjectPos = CameraObject.transform.position;
+        transform.RotateAround(CameraObjectPos, Vector3.up, angle);
+    }
+}
