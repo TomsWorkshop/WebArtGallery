b70be2bc6281fb4dba4299cea23bd89e0047931b
calm0815(kuwano) <kuwano.tomohiro455@mail.kyutech.jp>
2020-05-27 21:35:56 +0900

    add RPY

--- Assets/Scripts/UI/Debug/getParam.cs
+++ Assets/Scripts/UI/Debug/getParam.cs
@@ -1,19 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class getParam : MonoBehaviour
 {
-    private Vector3 m_accel;
-    private Gyroscope m_gyro;
+    public Vector3 m_accel;
+    public Gyroscope m_gyro;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_accel = Input.acceleration;
-        m_gyro  = Input.gyro;
-        m_gyro.enabled = true;
+        Input.gyro.enabled = true;
     }
 
     private void Update()
@@ -22,16 +17,21 @@ public class getParam : MonoBehaviour
         m_gyro  = Input.gyro;
     }
 
-    private void OnGUI() {
+    private void OnGUI()
+    {
         var AccRect = new Rect(30, 30, 500, 50);
-        var GyroRect = new Rect(30,60, 500, 50);
-        var AttitudeRect = new Rect(30,90, 500, 50);
+        var GyroRect = new Rect(30, 60, 500, 50);
+        var AttitudeRect = new Rect(30, 90, 500, 50);
+        var RPYRect = new Rect(30, 120, 500, 50);
+
         GUI.skin.label.fontSize = 20;
         GUI.Label(AccRect, string.Format("Acceleration : X={0:F2}, Y={1:F2}, Z={2:F2}",
             m_accel.x, m_accel.y, m_accel.z));
         GUI.Label(GyroRect, string.Format("Gyro : X={0:F2}, Y={1:F2}, Z={2:F2}",
             m_gyro.rotationRate.x, m_gyro.rotationRate.y, m_gyro.rotationRate.z));
-        GUI.Label(AttitudeRect, string.Format("Attitude : X={0:F2}, Y={1:F2}, Z={2:F2}",
-            m_gyro.attitude.x, m_gyro.attitude.y, m_gyro.attitude.z));
+        GUI.Label(AttitudeRect, string.Format("Attitude : X={0:F2}, Y={1:F2}, Z={2:F2}, W={2:F2}",
+            m_gyro.attitude.x, m_gyro.attitude.y, m_gyro.attitude.z, m_gyro.attitude.w));
+        GUI.Label(RPYRect, string.Format("RPY : Roll={0:F2}, Pitch={1:F2}, Yaw={2:F2}",
+            m_gyro.attitude.eulerAngles.x, m_gyro.attitude.eulerAngles.y, m_gyro.attitude.eulerAngles.z));
     }
 }
