fb26f240ade244d5ff9f41a21eece8d8d0bf03bb
chii777 <ma_ksks40x25@yahoo.co.jp>
2020-05-28 19:10:23 +0900

    add text change with collision2
    衝突の範囲を拡大

--- Assets/Scripts/Model/description1.cs
+++ Assets/Scripts/Model/description1.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Globalization;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class description1 : MonoBehaviour
 {
-    private Text targetText;
+    public Text targetText;
 
-    void substitude()
+    
+    void OnCollisionStay(Collision collision)　　　　　　//物体に衝突している間
     {
-        this.targetText = this.GetComponent<Text>();
-        this.targetText.text = "ChangeText";
+          
+            Debug.Log("hit");     　//consoleに表示
+           // this.targetText = this.GetComponent<Text>();
+            this.targetText.text = "ChangeText";　　　 //表示したい文章
+    }
+
+    void OnCollisionExit(Collision collision)　　　　　//物体から離れたとき
+    {
+
+        this.targetText.text = " ";　　　　　　　　　　　//textに空白を入力
+
     }
 }
