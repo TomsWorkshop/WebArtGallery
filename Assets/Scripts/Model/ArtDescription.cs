69ab2a961e8877e9e1a0b978551dfda19284a173
chii777 <ma_ksks40x25@yahoo.co.jp>
2020-05-30 18:50:44 +0900

    整理したんご

--- Assets/Scripts/Model/ArtDescription.cs
+++ Assets/Scripts/Model/ArtDescription.cs
@@ -7,20 +7,15 @@ using UnityEngine.UI;
 
 public class ArtDescription : MonoBehaviour
 {
-    [SerializeField] private Text targetText;
+    // [SerializeField] private Text targetText;
     [SerializeField] private GameObject DescriptionPanel;
-
-    private void Start()
-    {
-        // DescriptionPanel = GameObject.Find ("DescriptionBackground");
-    }
 
     //void OnCollisionStay(Collision collision)　　　　　　//物体に衝突している間(貫通できない) <= ダメ
     void OnTriggerStay(Collider collision)                 //貫通できる
     {
         Debug.Log("Hit !");     　//consoleに表示
         DescriptionPanel.SetActive(true);
-        this.targetText.text = "ChangeText";　　　 //表示したい文章
+        DescriptionPanel.GetComponentInChildren<Text>().text = "ChangeText";　　　 //表示したい文章
     }
 
     //void OnCollisionExit(Collision collision)　　　　　//物体から離れたとき(貫通できない)     <= ダメ
@@ -28,6 +23,6 @@ public class ArtDescription : MonoBehaviour
     {
         Debug.Log("Not Hit");     　//consoleに表示
         DescriptionPanel.SetActive(false);
-        this.targetText.text = "";　　　　　　　　　　　//textに空白を入力
+        DescriptionPanel.GetComponentInChildren<Text>().text = "";　　　　　　　　　　　//textに空白を入力
     }
 }
