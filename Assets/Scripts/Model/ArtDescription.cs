469e687edf789ca45fa7e4ad5bd7f179d88702d6
chii777 <ma_ksks40x25@yahoo.co.jp>
2020-05-30 18:31:41 +0900

    rename

---
+++ Assets/Scripts/Model/ArtDescription.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ArtDescription : MonoBehaviour
+{
+    [SerializedField] private Text targetText;
+    private GameObject DescriptionPanel;
+
+    private void Start()
+    {
+        DescriptionPanel = GameObject.Find ("DescriptionBackground");
+    }
+
+    //void OnCollisionStay(Collision collision)　　　　　　//物体に衝突している間(貫通できない) <= ダメ
+    void OnTriggerStay(Collider collision)                 //貫通できる
+    {
+        Debug.Log("Hit !");     　//consoleに表示
+        DescriptionPanel.enabled = true;
+        this.targetText.text = "ChangeText";　　　 //表示したい文章
+    }
+
+    //void OnCollisionExit(Collision collision)　　　　　//物体から離れたとき(貫通できない)     <= ダメ
+    void OnTriggerExit(Collider collision)　　　　　　　　//貫通できる
+    {
+        Debug.Log("Not Hit");     　//consoleに表示
+        // DescriptionPanel.enabled = false;
+        this.targetText.text = "";　　　　　　　　　　　//textに空白を入力
+    }
+}
