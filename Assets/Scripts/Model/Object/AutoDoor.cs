504102ff08240978536bdbd182e04f36fcb99c75
chii777 <ma_ksks40x25@yahoo.co.jp>
2020-05-31 19:55:45 +0900

    development process(autodoor)

--- Assets/Scripts/Model/Object/AutoDoor.cs
+++ Assets/Scripts/Model/Object/AutoDoor.cs
@@ -5,26 +5,9 @@ using UnityEngine;
 
 public class AutoDoor : MonoBehaviour
 {
-    /*
-    public Rigidbody rigidbody;
-   
-    void Start()
-     {
-        rigidbody = this.GetComponent<Rigidbody>();
-     }
-    // Update is called once per frame
-    */
 
-    /* 
-     void Update()
-      {
-
-             this.transform.position += new Vector3(0.01f, 0, 0);
-                      //endlessでどっかいくーｗ
-      }
-     */
-
-    /*
+  // SphereCollider sensor = this.Sensor.GetComponent<SphereCollider>(); 
+   /*
 void OnTriggerStay(Collider collision)
 {            
    //float x = 3.5;
@@ -41,19 +24,15 @@ void OntriggerExit(Collider collision)
 
 
 
-  void Update()
-    {
-       // void OnCollisionEnter (Collision collision) 
-       // { 
-             
-            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(3.5f, 1.25f, 0), 0.06f);
-    
+ void Update()
+   {
+       // void OnTriggerEnter (Collider collision) 
+       // {             
+            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(2.0f, 0, 0), 0.06f);   
           //  transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(1.6f, 1.25f, 0), 0.06f);
-
 //        }
-    
-    }
-  
+   }
+
 }
 
 
