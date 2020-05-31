c1c8fc7ce01fd5da231d9b464dafb6cb86dd7dad
calm0815(kuwano) <kuwano.tomohiro455@mail.kyutech.jp>
2020-05-31 18:44:52 +0900

    add Tweet function

---
+++ Assets/Scripts/UI/main/TweetButton.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// ツイート機能のサンプル
+/// </summary>
+public class TweetButton : MonoBehaviour
+{
+    // 各種パラメーターはインスペクターから設定する
+    [SerializeField] Button tweetButton;                        // ツイートするボタン
+    [SerializeField] string text = "ツイート機能のテスト中";    // ツイートに挿入するテキスト
+    [SerializeField] string linkUrl = "https://tomsworkshop.github.io/WebArtGalleryApp/index.html";   // ツイートに挿入するURL
+    [SerializeField] string hashtags = "Unity,WebArtGallery";        // ツイートに挿入するハッシュタグ
+
+    // ツイート画面を開く
+    private void Tweeting ()
+    {
+        var url = "https://twitter.com/intent/tweet?"
+            + "text=" + text
+            + "&url=" + linkUrl
+            + "&hashtags=" + hashtags;
+
+        #if UNITY_EDITOR
+            Application.OpenURL ( url );
+        #elif UNITY_WEBGL
+            // WebGLの場合は、ゲームプレイ画面と同じウィンドウでツイート画面が開かないよう、処理を変える
+            Application.ExternalEval(string.Format("window.open('{0}','_blank')", url));
+        #else
+            Application.OpenURL(url);
+        #endif
+    }
+
+    // UIボタンのクリックでツイート画面を開く場合
+    private void Start ()
+    {
+        tweetButton.onClick.AddListener ( () =>
+        {
+            Tweeting ();
+        } );
+    }
+
+    // マウスの右クリックでツイート画面を開く場合
+    private void Update ()
+    {
+        if ( Input.GetMouseButtonDown ( 1 ) )
+        {
+            Tweeting ();
+        }
+    }
+}
