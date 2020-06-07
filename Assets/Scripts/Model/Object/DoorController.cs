1324a1b32395f340b733ab2fa4737b1a03070fd3
calm0815(kuwano) <kuwano.tomohiro455@mail.kyutech.jp>
2020-06-07 21:56:48 +0900

    test

--- Assets/Scripts/Model/Object/DoorController.cs
+++ Assets/Scripts/Model/Object/DoorController.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     [SerializeField] private GameObject RightDoor;
+    private double RightDoorRadius;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        RightDoorRadius = RightDoor.transform.localScale.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RightDoor.transform.Rotate(Vector3.up * Time.deltaTime*200);
     }
 }
