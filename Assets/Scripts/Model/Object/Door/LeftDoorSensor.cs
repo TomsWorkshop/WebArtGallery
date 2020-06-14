cac9e6081ef35fcf3e4751f3605488b4245034f8
chii777 <ma_ksks40x25@yahoo.co.jp>
2020-06-14 20:55:18 +0900

    add autodoor
    左右のドアで分けて作り直しました。

---
+++ Assets/Scripts/Model/Object/Door/LeftDoorSensor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LeftDoorSensor : MonoBehaviour
+{
+    [SerializeField] private GameObject controller;
+
+    void OnTriggerStay(Collider collision)
+    {
+        controller.GetComponent<LeftDoorController>().isSensorOn = true;
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        controller.GetComponent<LeftDoorController>().isSensorOn = false;
+    }
+}
---
+++ Assets/Scripts/Model/Object/Door/RightDoorSensor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RightDoorSensor : MonoBehaviour
+{
+    [SerializeField] private GameObject controller;
+
+    void OnTriggerStay(Collider collision)
+    {
+        controller.GetComponent<RightDoorController>().isSensorOn = true;
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        controller.GetComponent<RightDoorController>().isSensorOn = false;
+    }
+}
